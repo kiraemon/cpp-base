c94b6783416ef0c000c93958db6e3380f2d983bc
Ryuji Kira <kira.ryuji1024@gmail.com>
2022-11-14 23:46:35 +0900

    in,outの記述/formatはbundleのものを使用

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main() {
+  int N = 0;
+  std::cin >> N;
+
+  std::vector<int> outputs;
+  for (int i = 0; i < N; ++i) {
+    int input = 0;
+    std::cin >> input;
+    outputs.push_back(input);
+  }
+
+  for (auto& output : outputs) {
+    std::cout << output << " ";
+  }
+
+  std::cout << std::endl;
 }
